9bca755121b42ffd3cf02bc56592f35a4480053f
qiaoshunho <285082071@qq.com>
2020-01-16 12:50:22 +0800

    Update DemoApplication.java
    12：50

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		//自己测试
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
